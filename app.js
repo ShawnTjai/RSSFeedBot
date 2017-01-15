152a1d337268becd41b3dad123c98f7e9956723c
Shawn Tjai <v-shtjai@microsoft.com>
2017-01-16 01:16:55 +0800

    Retrieval of RSS Feed from LUIS Intent's URL Entity

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 // Reference the packages we require so that we can use them in creating the bot
 var restify = require('restify');
 var builder = require('botbuilder');
+var feedparser = require('feedparser-promised');
 
 //=========================================================
 // Bot Setup
 //=========================================================
+
+//Setup LUIS
+var luisRecognizer = new builder.LuisRecognizer('https://api.projectoxford.ai/luis/v2.0/apps/ff6b0484-c17d-4fa8-a749-5ee96b2cf84e?subscription-key=b7dbaddbc50940a7b146c5e6447c30c4&verbose=true');
+var intentDialog = new builder.IntentDialog({recognizers: [luisRecognizer]});
 
 // Setup Restify Server
 // Listen for any activity on port 3978 of our local server
@@ -15,19 +20,73 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID || "f9d2da2c-7d16-48d0-86d0-f94da6ad009f",
-    appPassword: process.env.MICROSOFT_APP_PASSWORD || "hczyvsw66mZO4U3DCkADY6M"
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
+
 var bot = new builder.UniversalBot(connector);
-// If a Post request is made to /api/messages on port 3978 of our local server, then we pass it to the bot connector to handle
 server.post('/api/messages', connector.listen());
+
+//LUIS Intents
+intentDialog.matches('genConvo', '/initiateConversation')
+.matches('loadRSS', '/getRSS')
+.matches()
+    .matches('genVulgarity', '/avoidVulgar')
+    .onDefault(builder.DialogAction.send("Sorry, I didn't understand what you said."));
 
 //=========================================================
 // Bots Dialogs
 //=========================================================
 
-// This is called the root dialog. It is the first point of entry for any message the bot receives
-bot.dialog('/', function (session) {
-    // Send 'hello world' to the user
-    session.send("Hey Sup");
-});
+bot.dialog('/', intentDialog);
+
+bot.dialog('/initiateConversation', [function (session){
+        session.send("Hello there! I'm a RSS Feed Bot.\n\nSend me a URL to get started!");
+    }
+]);
+
+bot.dialog('/getRSS', function (session, args){
+        console.log(session.message.text);
+        var rssURL = builder.EntityRecognizer.findEntity(args.entities, 'builtin.url');
+
+        console.log(rssURL);
+        if (rssURL) {
+            var url = rssURL.entity;
+            session.send('Looking for RSS feed of \'%s\'...', url);
+
+            var count = 0;
+            var finalMessage = "RSS Feed for "+url+"\n\n";
+
+            feedparser.parse(url).then(function (items) {
+                try{
+                    items.forEach(function (item) {
+                        if(count<20){
+                            count++;
+                            console.log('title: ', item.title);
+                            finalMessage = finalMessage + "Article "+count+": "+item.title+"\n\n";                        
+                        }
+                    });
+
+                session.send(finalMessage);
+
+                } catch (err){
+                    console.log(err);
+                }
+            }).catch(function (error) {
+                console.log('error: ', error);
+            });
+        }
+    }
+);
+
+bot.dialog('/avoidVulgar', [function (session){
+        builder.Prompts.choice(session, "That's rude, take that back!", "Sorry|No");
+    }, function (session, results){
+        if (results.response && results.response.entity !== 'No') {
+            session.sendTyping();
+            session.endDialog("Apology accepted.");
+        } else {
+            session.endDialog("K. Talk to me when you've learnt some manners.");
+        }
+    }
+]);
