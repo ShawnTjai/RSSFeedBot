9f3bed0dda96f1de30c6302616ea8dfeb74315d0
v-shtjai <v-shtjai@microsoft.com>
2017-01-16 14:14:43 +0800

    RSS Feeds display as cards

--- app.js
+++ app.js
@@ -29,8 +29,7 @@ server.post('/api/messages', connector.listen());
 
 //LUIS Intents
 intentDialog.matches('genConvo', '/initiateConversation')
-.matches('loadRSS', '/getRSS')
-.matches()
+    .matches('loadRSS', '/getRSS')
     .matches('genVulgarity', '/avoidVulgar')
     .onDefault(builder.DialogAction.send("Sorry, I didn't understand what you said."));
 
@@ -40,10 +39,10 @@ intentDialog.matches('genConvo', '/initiateConversation')
 
 bot.dialog('/', intentDialog);
 
-bot.dialog('/initiateConversation', [function (session){
-        session.send("Hello there! I'm a RSS Feed Bot.\n\nSend me a URL to get started!");
+bot.dialog('/initiateConversation', function (session){
+        session.endDialog("Hello there! I'm a RSS Feed Bot.\n\nSend me a URL to get started!");
     }
-]);
+);
 
 bot.dialog('/getRSS', function (session, args){
         console.log(session.message.text);
@@ -56,18 +55,35 @@ bot.dialog('/getRSS', function (session, args){
 
             var count = 0;
             var finalMessage = "RSS Feed for "+url+"\n\n";
+            var cards = [];
 
             feedparser.parse(url).then(function (items) {
                 try{
                     items.forEach(function (item) {
-                        if(count<20){
+                        if(count<5){
                             count++;
                             console.log('title: ', item.title);
-                            finalMessage = finalMessage + "Article "+count+": "+item.title+"\n\n";                        
+                            finalMessage = finalMessage + "Article "+count+": "+item.title+"\n\n";  
+
+                            var tmp = new builder.HeroCard(session)
+                            .title(item.title)
+                            .text(item.summary)
+                            .images([
+                                builder.CardImage.create(session, item.image.url)
+                            ])
+                            .buttons([
+                                builder.CardAction.openUrl(session, item.link, 'Read More')
+                            ]);
+
+                            cards[count] = tmp;
                         }
                     });
 
-                session.send(finalMessage);
+                var reply = new builder.Message(session)
+                    .attachmentLayout(builder.AttachmentLayout.carousel)
+                    .attachments(cards);
+
+                session.endDialog(reply);
 
                 } catch (err){
                     console.log(err);
@@ -90,3 +106,51 @@ bot.dialog('/avoidVulgar', [function (session){
         }
     }
 ]);
+
+function getCards(session){
+    return [
+        new builder.HeroCard(session)
+            .title('Azure Storage')
+            .subtitle('Massively scalable cloud storage for your applications')
+            .text('Store and help protect your data. Get durable, highly available data storage across the globe and pay only for what you use.')
+            .images([
+                builder.CardImage.create(session, 'https://acom.azurecomcdn.net/80C57D/cdn/mediahandler/docarticles/dpsmedia-prod/azure.microsoft.com/en-us/documentation/articles/storage-introduction/20160801042915/storage-concepts.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/storage/', 'Learn More')
+            ]),
+
+        new builder.ThumbnailCard(session)
+            .title('DocumentDB')
+            .subtitle('Blazing fast, planet-scale NoSQL')
+            .text('NoSQL service for highly available, globally distributed apps—take full advantage of SQL and JavaScript over document and key-value data without the hassles of on-premises or virtual machine-based cloud database options.')
+            .images([
+                builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/29f4/beb4b953-ab91-4a31-b16a-71fb6d6829f4/WhatisAzureDocumentDB_960.jpg')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/documentdb/', 'Learn More')
+            ]),
+
+        new builder.HeroCard(session)
+            .title('Azure Functions')
+            .subtitle('Process events with serverless code')
+            .text('Azure Functions is a serverless event driven experience that extends the existing Azure App Service platform. These nano-services can scale based on demand and you pay only for the resources you consume.')
+            .images([
+                builder.CardImage.create(session, 'https://azurecomcdn.azureedge.net/cvt-8636d9bb8d979834d655a5d39d1b4e86b12956a2bcfdb8beb04730b6daac1b86/images/page/services/functions/azure-functions-screenshot.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/functions/', 'Learn More')
+            ]),
+
+        new builder.ThumbnailCard(session)
+            .title('Cognitive Services')
+            .subtitle('Build powerful intelligence into your applications to enable natural and contextual interactions')
+            .text('Enable natural and contextual interaction with tools that augment users\' experiences using the power of machine-based intelligence. Tap into an ever-growing collection of powerful artificial intelligence algorithms for vision, speech, language, and knowledge.')
+            .images([
+                builder.CardImage.create(session, 'https://azurecomcdn.azureedge.net/cvt-8636d9bb8d979834d655a5d39d1b4e86b12956a2bcfdb8beb04730b6daac1b86/images/page/services/functions/azure-functions-screenshot.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/functions/', 'Learn More')
+            ])
+    ];
+}
