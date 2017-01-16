c760dd21c0f1f957de9f11f3c614798b4c7371b2
v-shtjai <v-shtjai@microsoft.com>
2017-01-16 17:25:16 +0800

    Translation from source language to user indicated language code

--- app.js
+++ app.js
@@ -2,10 +2,17 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 var feedparser = require('feedparser-promised');
+var validUrl = require('valid-url');
+var translator = require('mstranslator');
 
 //=========================================================
 // Bot Setup
 //=========================================================
+
+//Setup Translator
+var client = new translator({
+  api_key: "97509730891a4301b8d176b9fc8ba3e5" // use this for the new token API. 
+}, true);
 
 //Setup LUIS
 var luisRecognizer = new builder.LuisRecognizer('https://api.projectoxford.ai/luis/v2.0/apps/ff6b0484-c17d-4fa8-a749-5ee96b2cf84e?subscription-key=b7dbaddbc50940a7b146c5e6447c30c4&verbose=true');
@@ -44,53 +51,112 @@ bot.dialog('/initiateConversation', function (session){
     }
 );
 
+var cards = [];
+
 bot.dialog('/getRSS', function (session, args){
+        cards = [];
         console.log(session.message.text);
         var rssURL = builder.EntityRecognizer.findEntity(args.entities, 'builtin.url');
+        var translateLang = builder.EntityRecognizer.findEntity(args.entities, 'language');
 
         console.log(rssURL);
         if (rssURL) {
             var url = rssURL.entity;
-            session.send('Looking for RSS feed of \'%s\'...', url);
+            
+            if(validUrl.isUri(url)){
+                session.send('Looking for RSS feed of \'%s\'...', url);
 
-            var count = 0;
-            var finalMessage = "RSS Feed for "+url+"\n\n";
-            var cards = [];
+                var count = 0;
+                var cardCount = 0;
+                var finalMessage = "RSS Feed for "+url+"\n\n";
+                var obj = {};
 
-            feedparser.parse(url).then(function (items) {
-                try{
-                    items.forEach(function (item) {
-                        if(count<5){
-                            count++;
-                            console.log('title: ', item.title);
-                            finalMessage = finalMessage + "Article "+count+": "+item.title+"\n\n";  
+                client.getLanguagesForTranslate(function(err, data){
+                    console.log(data);
+                });
 
-                            var tmp = new builder.HeroCard(session)
-                            .title(item.title)
-                            .text(item.summary)
-                            .images([
-                                builder.CardImage.create(session, item.image.url)
-                            ])
-                            .buttons([
-                                builder.CardAction.openUrl(session, item.link, 'Read More')
-                            ]);
+                feedparser.parse(url).then(function (items) {
+                    try{
+                        items.forEach(function (item) {
+                            if(count<5){
+                                count++;
+                                finalMessage = finalMessage + "Article "+count+": "+item.title+"\n\n";
 
-                            cards[count] = tmp;
-                        }
-                    });
+                                if(translateLang){
+                                    obj.toLang = translateLang.entity;
+                                    var paramsLang = {
+                                        text: item.description
+                                    };
 
-                var reply = new builder.Message(session)
-                    .attachmentLayout(builder.AttachmentLayout.carousel)
-                    .attachments(cards);
+                                    client.detect(paramsLang, function(err, data){
+                                        obj.oriLang = data;
+                                        console.log("Detected Language: "+obj.oriLang);
+                                    });
 
-                session.endDialog(reply);
+                                    console.log("Translating to "+obj.toLang);
 
-                } catch (err){
-                    console.log(err);
-                }
-            }).catch(function (error) {
-                console.log('error: ', error);
-            });
+                                    var paramsDesc = {
+                                        text: item.description,
+                                        from: obj.oriLang,
+                                        to: obj.toLang
+                                    };
+
+                                    var paramsTitle = {
+                                        text: item.title,
+                                        from: obj.oriLang,
+                                        to: obj.toLang
+                                    };
+
+                                    client.translate(paramsDesc, function(err, data){
+                                        obj.desc = data;
+                                        console.log(obj.desc);
+
+                                        client.translate(paramsTitle, function(err, data){
+                                            obj.title = data;
+                                            console.log(obj.title);
+
+                                            console.log("Article "+cardCount+": "+obj.title);
+
+                                            var tmp = new builder.HeroCard(session)
+                                                .title(obj.title)
+                                                .subtitle("Translated from "+obj.oriLang+" to Korean")
+                                                .text(obj.description)
+                                                .images([
+                                                    builder.CardImage.create(session, item.image.url)
+                                                ])
+                                                .buttons([
+                                                    builder.CardAction.openUrl(session, item.link, 'Read More')
+                                                ]);
+
+                                            cards[cardCount] = tmp;
+                                            cardCount++;
+
+                                            if(cardCount == 5){
+                                                var reply = new builder.Message(session)
+                                                    .attachmentLayout(builder.AttachmentLayout.carousel)
+                                                    .attachments(cards);
+
+                                                session.endDialog(reply);
+                                            }
+                                        });
+                                    });
+                                } else {
+                                    obj.title = item.title;
+                                    obj.description = item.description;
+                                }
+                            }
+                        });
+                        console.log(finalMessage);  
+                    } catch (err){
+                        console.log(err);
+                    }
+                }).catch(function (error) {
+                    session.endDialog("There is an error with your URL. Please enter a valid RSS feed.");
+                    console.log('error: ', error);
+                });
+            } else {
+                session.endDialog("Please enter a valid RSS url.");
+            }
         }
     }
 );
@@ -106,51 +172,3 @@ bot.dialog('/avoidVulgar', [function (session){
         }
     }
 ]);
-
-function getCards(session){
-    return [
-        new builder.HeroCard(session)
-            .title('Azure Storage')
-            .subtitle('Massively scalable cloud storage for your applications')
-            .text('Store and help protect your data. Get durable, highly available data storage across the globe and pay only for what you use.')
-            .images([
-                builder.CardImage.create(session, 'https://acom.azurecomcdn.net/80C57D/cdn/mediahandler/docarticles/dpsmedia-prod/azure.microsoft.com/en-us/documentation/articles/storage-introduction/20160801042915/storage-concepts.png')
-            ])
-            .buttons([
-                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/storage/', 'Learn More')
-            ]),
-
-        new builder.ThumbnailCard(session)
-            .title('DocumentDB')
-            .subtitle('Blazing fast, planet-scale NoSQL')
-            .text('NoSQL service for highly available, globally distributed apps—take full advantage of SQL and JavaScript over document and key-value data without the hassles of on-premises or virtual machine-based cloud database options.')
-            .images([
-                builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/29f4/beb4b953-ab91-4a31-b16a-71fb6d6829f4/WhatisAzureDocumentDB_960.jpg')
-            ])
-            .buttons([
-                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/documentdb/', 'Learn More')
-            ]),
-
-        new builder.HeroCard(session)
-            .title('Azure Functions')
-            .subtitle('Process events with serverless code')
-            .text('Azure Functions is a serverless event driven experience that extends the existing Azure App Service platform. These nano-services can scale based on demand and you pay only for the resources you consume.')
-            .images([
-                builder.CardImage.create(session, 'https://azurecomcdn.azureedge.net/cvt-8636d9bb8d979834d655a5d39d1b4e86b12956a2bcfdb8beb04730b6daac1b86/images/page/services/functions/azure-functions-screenshot.png')
-            ])
-            .buttons([
-                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/functions/', 'Learn More')
-            ]),
-
-        new builder.ThumbnailCard(session)
-            .title('Cognitive Services')
-            .subtitle('Build powerful intelligence into your applications to enable natural and contextual interactions')
-            .text('Enable natural and contextual interaction with tools that augment users\' experiences using the power of machine-based intelligence. Tap into an ever-growing collection of powerful artificial intelligence algorithms for vision, speech, language, and knowledge.')
-            .images([
-                builder.CardImage.create(session, 'https://azurecomcdn.azureedge.net/cvt-8636d9bb8d979834d655a5d39d1b4e86b12956a2bcfdb8beb04730b6daac1b86/images/page/services/functions/azure-functions-screenshot.png')
-            ])
-            .buttons([
-                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/functions/', 'Learn More')
-            ])
-    ];
-}
