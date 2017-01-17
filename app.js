3fc7fedbb9a58f7b18063d16d27924df9a586c6b
v-shtjai <v-shtjai@microsoft.com>
2017-01-17 13:47:27 +0800

    New LUIS entities for language list as well as adding nested translation

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-// Reference the packages we require so that we can use them in creating the bot
+// References
 var restify = require('restify');
 var builder = require('botbuilder');
 var feedparser = require('feedparser-promised');
 var validUrl = require('valid-url');
 var translator = require('mstranslator');
-
-//=========================================================
-// Bot Setup
-//=========================================================
 
 //Setup Translator
 var client = new translator({
@@ -18,14 +14,13 @@ var client = new translator({
 var luisRecognizer = new builder.LuisRecognizer('https://api.projectoxford.ai/luis/v2.0/apps/ff6b0484-c17d-4fa8-a749-5ee96b2cf84e?subscription-key=b7dbaddbc50940a7b146c5e6447c30c4&verbose=true');
 var intentDialog = new builder.IntentDialog({recognizers: [luisRecognizer]});
 
-// Setup Restify Server
-// Listen for any activity on port 3978 of our local server
+// Listen on port 3978
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
 
-// Create chat bot
+// Initialise Connector
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
@@ -38,11 +33,10 @@ server.post('/api/messages', connector.listen());
 intentDialog.matches('genConvo', '/initiateConversation')
     .matches('loadRSS', '/getRSS')
     .matches('genVulgarity', '/avoidVulgar')
+    .matches('getLanguages','/availableLanguages')
     .onDefault(builder.DialogAction.send("Sorry, I didn't understand what you said."));
 
-//=========================================================
-// Bots Dialogs
-//=========================================================
+// Bot Dialogs
 
 bot.dialog('/', intentDialog);
 
@@ -51,7 +45,15 @@ bot.dialog('/initiateConversation', function (session){
     }
 );
 
+bot.dialog('/availableLanguages', function (session){
+        client.getLanguagesForTranslate(function(err, data){
+            session.endDialog("Here are the available languages:\n\n(Refer to: https://www.loc.gov/standards/iso639-2/php/code_list.php)\n\n"+data);
+        });
+    }
+);
+
 var cards = [];
+var numOfArticles = 5;//Change this value to choose num of articles
 
 bot.dialog('/getRSS', function (session, args){
         cards = [];
@@ -71,14 +73,10 @@ bot.dialog('/getRSS', function (session, args){
                 var finalMessage = "RSS Feed for "+url+"\n\n";
                 var obj = {};
 
-                client.getLanguagesForTranslate(function(err, data){
-                    //console.log(data);
-                });
-
                 feedparser.parse(url).then(function (items) {
                     try{
                         items.forEach(function (item) {
-                            if(count<5){
+                            if(count<numOfArticles){
                                 count++;
                                 finalMessage = finalMessage + "Article "+count+": "+item.title+"\n\n";
 
@@ -92,62 +90,60 @@ bot.dialog('/getRSS', function (session, args){
                                     client.detect(paramsLang, function(err, data){
                                         obj.oriLang = data;
                                         console.log("Detected Language: "+obj.oriLang);
-                                    });
 
-                                    console.log("Translating to "+obj.toLang);
+                                        var paramsDesc = {
+                                            text: item.description,
+                                            from: obj.oriLang,
+                                            to: obj.toLang
+                                        };
 
-                                    var paramsDesc = {
-                                        text: item.description,
-                                        from: obj.oriLang,
-                                        to: obj.toLang
-                                    };
+                                        var paramsTitle = {
+                                            text: item.title,
+                                            from: obj.oriLang,
+                                            to: obj.toLang
+                                        };
 
-                                    var paramsTitle = {
-                                        text: item.title,
-                                        from: obj.oriLang,
-                                        to: obj.toLang
-                                    };
+                                        client.translate(paramsDesc, function(err, data){
+                                            obj.desc = data;
+                                            console.log("Translating to "+obj.toLang);
+                                            console.log(obj.desc);
 
-                                    client.translate(paramsDesc, function(err, data){
-                                        obj.desc = data;
-                                        console.log(obj.desc);
+                                            client.translate(paramsTitle, function(err, data){
+                                                obj.title = data;
+                                                console.log(obj.title);
 
-                                        client.translate(paramsTitle, function(err, data){
-                                            obj.title = data;
-                                            console.log(obj.title);
+                                                console.log("Article "+cardCount+": "+obj.title);
 
-                                            console.log("Article "+cardCount+": "+obj.title);
+                                                var tmp = new builder.HeroCard(session)
+                                                    .title(obj.title)
+                                                    .subtitle("Translated from "+obj.oriLang+" to "+obj.toLang)
+                                                    .text(obj.description)
+                                                    .images([
+                                                        builder.CardImage.create(session, item.image.url)
+                                                    ])
+                                                    .buttons([
+                                                        builder.CardAction.openUrl(session, item.link, 'Read More')
+                                                    ]);
 
-                                            var tmp = new builder.HeroCard(session)
-                                                .title(obj.title)
-                                                .subtitle("Translated from "+obj.oriLang+" to "+obj.toLang)
-                                                .text(obj.description)
-                                                .images([
-                                                    builder.CardImage.create(session, item.image.url)
-                                                ])
-                                                .buttons([
-                                                    builder.CardAction.openUrl(session, item.link, 'Read More')
-                                                ]);
+                                                cards[cardCount] = tmp;
+                                                cardCount++;
 
-                                            cards[cardCount] = tmp;
-                                            cardCount++;
+                                                if(cardCount == numOfArticles){
+                                                    var reply = new builder.Message(session)
+                                                        .attachmentLayout(builder.AttachmentLayout.carousel)
+                                                        .attachments(cards);
 
-                                            if(cardCount == 5){
-                                                var reply = new builder.Message(session)
-                                                    .attachmentLayout(builder.AttachmentLayout.carousel)
-                                                    .attachments(cards);
-
-                                                session.endDialog(reply);
-                                            }
+                                                    session.endDialog(reply);
+                                                }
+                                            });
                                         });
-                                    });
+                                    });                                    
                                 } else {
                                     obj.title = item.title;
                                     obj.description = item.description;
 
                                     var tmp = new builder.HeroCard(session)
                                                 .title(obj.title)
-                                                .subtitle("Translated from "+obj.oriLang+" to "+obj.toLang)
                                                 .text(obj.description)
                                                 .images([
                                                     builder.CardImage.create(session, item.image.url)
@@ -160,6 +156,7 @@ bot.dialog('/getRSS', function (session, args){
                                 }
                             }
                         });
+
                         if(!translateLang){
                             var reply = new builder.Message(session)
                                         .attachmentLayout(builder.AttachmentLayout.carousel)
